a97191d37526c54c41cfa88e2956fc564a0daed2
lucascarv <lucascarvalhio077@gmail.com>
2025-03-31 15:50:56 -0300

    finished form and history component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,7 @@
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 
 export function Transactions(){
@@ -7,6 +9,35 @@ export function Transactions(){
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <SearchForm/>
+
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de Site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
